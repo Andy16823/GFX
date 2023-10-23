c9bcd16777abe3cf215fb101f63d4d47ef3f7d46
Andy16823 <wagnerandreas17@yahoo.de>
2023-10-23 11:28:42 +0200

    Upload v 0.0.1
    First Upload

---
+++ GFX/GFX 0.0.1/Genesis/Genesis/Core/Utils.cs
@@ -0,0 +1,38 @@
+﻿using Genesis.Graphics;
+using Genesis.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Genesis.Core
+{
+    public class Utils
+    {
+        public static long GetCurrentTimeMillis()
+        {
+            return DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+        }
+
+        public static float GetStringWidht(String text, float fontSize, float spacing)
+        {
+            int chars = text.Length;
+            float baseWidth = chars * fontSize;
+            float spaceWidth = (float)(fontSize * spacing);
+            float spacingWidth = spaceWidth * (chars - 1);
+            return baseWidth - spacingWidth;
+        }
+
+        public static Rect GetStringBounds(Vec3 location, String text, float fontSize, float spacing)
+        {
+            Rect rect = new Rect();
+            rect.X = location.X;
+            rect.Y = location.Y;
+            rect.Width = GetStringWidht(text, fontSize, spacing);
+            rect.Height = fontSize;
+            return rect;
+        }
+    }
+}
