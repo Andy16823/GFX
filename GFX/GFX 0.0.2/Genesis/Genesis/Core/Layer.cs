faf92c5d38ffe22c61fe9cbc76bc77a6d69872e8
Andy16823 <wagnerandreas17@yahoo.de>
2023-10-24 09:25:30 +0200

    Small changes

---
+++ GFX/GFX 0.0.2/Genesis/Genesis/Core/Layer.cs
@@ -0,0 +1,59 @@
+﻿using Genesis.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genesis.Core
+{
+    public class Layer
+    {
+        public String Name { get; set; }
+        public List<GameElement> Elements { get; set; }
+
+        public Layer()
+        {
+            Elements= new List<GameElement>();
+        }
+
+        public Layer(String name)
+        {
+            this.Name = name;
+            Elements= new List<GameElement>();
+        }
+
+        public void Init(Game game, IRenderDevice renderDevice)
+        {
+            foreach (var item in Elements)
+            {
+                item.Init(game, renderDevice);
+            }
+        }
+
+        public void OnRender(Game game, IRenderDevice renderDevice)
+        {
+            foreach (var item in Elements)
+            {
+                item.OnRender(game, renderDevice);
+            }
+        }
+
+        public void OnUpdate(Game game, IRenderDevice renderDevice)
+        {
+            foreach (var item in Elements)
+            {
+                item.OnUpdate(game, renderDevice);
+            }
+        }
+
+        public void OnDestroy(Game game)
+        {
+            foreach (var item in Elements)
+            {
+                item.OnDestroy(game);
+            }
+        }
+
+    }
+}
---
+++ GFX/GFX 0.0.2/Genesis/Genesis/Graphics/Camera.cs
@@ -0,0 +1,40 @@
+﻿using Genesis.Core;
+using Genesis.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genesis.Graphics
+{
+    public enum CameraType
+    {
+        Ortho,
+        Perspective
+    }
+
+    public class Camera
+    {
+        public Vec3 Location { get; set; }
+        public Vec3 Size { get; set; }
+        public float Near { get; set; }
+        public float Far { get; set; }
+        public CameraType Type { get; set; }
+
+        public Camera(Vec3 location, Vec3 size, float near, float far)
+        {
+            Location = location;
+            Size = size;
+            Near = near;
+            Far = far;
+        }
+
+        public void LookAt(GameElement element)
+        {
+            this.Location.X = element.Location.X + element.Size.X / 2;
+            this.Location.Y = element.Location.Y + element.Size.Y / 2;
+        }
+
+    }
+}
