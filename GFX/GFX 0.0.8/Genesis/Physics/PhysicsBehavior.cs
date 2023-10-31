7f41cb53e46bafd456a74ccd310f161c3625ea44
Andy16823 <wagnerandreas17@yahoo.de>
2023-10-31 01:15:21 +0100

    Added Physics and Lightning

---
+++ GFX/GFX 0.0.8/Genesis/Physics/PhysicsBehavior.cs
@@ -0,0 +1,26 @@
+﻿using Genesis.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genesis.Physics
+{
+    public delegate void PhysicsBehaviorEvent(Scene scene, Game game, object collision);
+
+    public abstract class PhysicsBehavior : IGameBehavior
+    {
+        public PhysicsBehaviorEvent OnCollide;
+
+        public virtual void Collide(Scene scene, Game game, BulletSharp.RigidBody collisionObject)
+        {
+            if(this.OnCollide != null)
+            {
+                this.OnCollide(scene, game, collisionObject);
+            }
+        }
+
+        public abstract object GetPhysicsObject();
+    }
+}
