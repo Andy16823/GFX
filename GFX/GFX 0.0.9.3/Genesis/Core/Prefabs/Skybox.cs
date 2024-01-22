253c8e59f00a537a64d9ce223cb933a2c0840bdc
Andy16823 <wagnerandreas17@yahoo.de>
2024-01-22 10:31:18 +0100

    Update

---
+++ GFX/GFX 0.0.9.3/Genesis/Core/Prefabs/Skybox.cs
@@ -0,0 +1,17 @@
+﻿using Genesis.Core.Prefabs;
+using Genesis.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genesis.Core.Prefabs
+{
+    public class Skybox : Element3D
+    {
+        public Skybox(string name, string path, Vec3 location, Vec3 rotation, Vec3 scale) : base(name, path, location, rotation, scale)
+        {
+        }
+    }
+}
---
+++ GFX/GFX 0.0.9.3/Genesis/Graphics/Shapes/GlypheShape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genesis.Graphics.Shapes
+{
+    public class GlypheShape : Shapes.Shape
+    {
+        public override float[] GetShape()
+        {
+            float[] verticies =
+{
+                -0.5f, -0.5f, 0.0f,
+                -0.5f, 0.5f, 0.0f,
+                0.5f, 0.5f, 0.0f,
+
+                -0.5f, -0.5f, 0.0f,
+                0.5f, 0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+
+                0.0f, 1.0f,
+                0.0f, 0.0f,
+                1.0f, 0.0f,
+
+                0.0f, 1.0f,
+                1.0f, 0.0f,
+                1.0f, 1.0f
+            };
+            return verticies;
+        }
+    }
+}
