de34043add72649cf33f0727d673e4befc6864f9
Andy16823 <wagnerandreas17@yahoo.de>
2024-03-17 13:09:35 +0100

    Animation  Beta Update

---
+++ Source/Genesis/Core/Behaviors/3D/ThirdpersonCharacterController.cs
@@ -0,0 +1,293 @@
+﻿using BulletSharp.Math;
+using Genesis.Core.Behaviors.Physics3D;
+using Genesis.Core.GameElements;
+using Genesis.Graphics;
+using Genesis.Math;
+using Genesis.Physics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Genesis.Core.Behaviors._3D
+{
+    /// <summary>
+    /// Defines the stance of the character.
+    /// </summary>
+    public enum Stance
+    {
+        Walk,
+        Run,
+        Idle
+    }
+
+    /// <summary>
+    /// Controls the behavior of a third-person character in a game.
+    /// </summary>
+    public class ThirdpersonCharacterController : IGameBehavior
+    {
+        /// <summary>
+        /// Gets or sets the collider for the character.
+        /// </summary>
+        public CapsuleCollider Collider { get; set; }
+
+        /// <summary>
+        /// Gets or sets the speed of walking.
+        /// </summary>
+        public float WalkSpeed { get; set; } = 0.3f;
+
+        /// <summary>
+        /// Gets or sets the speed of running.
+        /// </summary>
+        public float RunSpeed { get; set; } = 0.7f;
+
+        /// <summary>
+        /// Gets or sets the speed of jumping.
+        /// </summary>
+        public float JumpSpeed { get; set; } = 1f;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the character is running.
+        /// </summary>
+        public bool IsRunning { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the character is colliding.
+        /// </summary>
+        public bool IsColliding { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the walking animation.
+        /// </summary>
+        public String WalkAnimation { get; set; } = "walk";
+
+        /// <summary>
+        /// Gets or sets the name of the idle animation.
+        /// </summary>
+        public String IdleAnimation { get; set; } = "idle";
+
+        /// <summary>
+        /// Gets or sets the name of the running animation.
+        /// </summary>
+        public String RunAnimation { get; set; } = "run";
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the mouse movement is inverted.
+        /// </summary>
+        public bool InvertMouse { get; set; }
+
+        /// <summary>
+        /// Gets or sets the stance of the character.
+        /// </summary>
+        public Stance Stance { get; set; }
+
+        private long jumpCooldown = 250;
+        private long lastJump = 0;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThirdpersonCharacterController"/> class.
+        /// </summary>
+        public ThirdpersonCharacterController()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates the physics for the character.
+        /// </summary>
+        /// <param name="handler">The physics handler.</param>
+        /// <param name="radius">The radius.</param>
+        /// <param name="height">The height.</param>
+        /// <param name="mass">The mass.</param>
+        public void CreatePhysics(PhysicHandler handler, float radius, float height, float mass)
+        {
+            this.CreatePhysics(handler, Vec3.Zero(), radius, height, mass);
+        }
+
+        /// <summary>
+        /// Creates the physics for the character.
+        /// </summary>
+        /// <param name="handler">The physics handler.</param>
+        /// <param name="offset">The offset.</param>
+        /// <param name="radius">The radius.</param>
+        /// <param name="height">The height.</param>
+        /// <param name="mass">The mass.</param>
+        public void CreatePhysics(PhysicHandler handler, Vec3 offset, float radius, float height, float mass)
+        {
+            this.Collider = new CapsuleCollider();
+            this.Parent.AddBehavior(this.Collider);
+            this.Collider.CreateRigidBody(handler, offset, radius, height, mass);
+            this.Collider.RigidBody.AngularFactor = new BulletSharp.Math.Vector3(0);
+            Collider.OnCollide += (sce, game, collisionObject) =>
+            {
+                IsColliding = true;
+            };
+        }
+
+        /// <summary>
+        /// Called when the game element is destroyed.
+        /// </summary>
+        /// <param name="game">The game instance.</param>
+        /// <param name="parent">The parent game element.</param>
+        public override void OnDestroy(Game game, GameElement parent)
+        {
+
+        }
+
+        /// <summary>
+        /// Called when the behavior is initialized.
+        /// </summary>
+        /// <param name="game">The game instance.</param>
+        /// <param name="parent">The parent game element.</param>
+        public override void OnInit(Game game, GameElement parent)
+        {
+
+        }
+
+        /// <summary>
+        /// Called when the game element should be rendered.
+        /// </summary>
+        /// <param name="game">The game instance.</param>
+        /// <param name="parent">The parent game element.</param>
+        public override void OnRender(Game game, GameElement parent)
+        {
+
+        }
+
+        /// <summary>
+        /// Called when the game element should be updated.
+        /// </summary>
+        /// <param name="game">The game instance.</param>
+        /// <param name="parent">The parent game element.</param>
+        public override void OnUpdate(Game game, GameElement parent)
+        {
+            // Convert the parent game element to an model game element
+            Model model = (Model)this.Parent;
+            model.AnimationSpeed = (float)game.DeltaTime * 0.001f;
+
+            // Set the model rotation
+            Vec3 currentCursoPos = Input.GetMousePos();
+            Input.SetCursorPos((int)(game.Viewport.Width / 2), (int)(game.Viewport.Height / 2), game.RenderDevice.GetHandle());
+            Vec3 diff = currentCursoPos - Input.GetMousePos();
+            if (this.InvertMouse)
+            {
+                model.Rotation.Y += (diff.X / 100);
+            }
+            else
+            {
+                model.Rotation.Y -= (diff.X / 100);
+
+            }
+
+            // Let the camera follow the player
+            PerspectiveCamera camera = (PerspectiveCamera)game.SelectedScene.Camera;
+            camera.Location = Utils.GetRelativePosition(model, new Vec3(0f, 2.5f, -4f));
+            camera.Rotation.X = -20f;
+            Utils.LookAt(camera, model.Location + new Vec3(0, 1, 0));
+
+
+            // Setup the Velocity for the player movement.
+            Vector3 velocity = new Vector3(0, Collider.RigidBody.LinearVelocity.Y, 0f);
+
+            // Setup the movement speed
+            float speed = (float)game.DeltaTime * WalkSpeed;
+
+            // Move the player
+            if (Input.IsKeyDown(Keys.W) || Input.IsKeyDown(Keys.S))
+            {
+                if (Input.IsKeyDown(Keys.W))
+                {
+                    if (Input.IsKeyDown(Keys.ShiftKey))
+                    {
+                        speed = (float)game.DeltaTime * RunSpeed;
+                        IsRunning = true;
+                    }
+                    var fwd = Utils.GetForwardDirection(model.Rotation);
+                    velocity += (fwd.ToBulletVec3() * speed);
+                }
+                else if (Input.IsKeyDown(Keys.S))
+                {
+                    var fwd = Utils.GetForwardDirection(model.Rotation);
+                    velocity -= (fwd.ToBulletVec3() * speed);
+                }
+                if (this.IsRunning)
+                {
+                    this.Stance = Stance.Run;
+
+                }
+                else
+                {
+                    this.Stance = Stance.Walk;
+
+                }
+            }
+            else
+            {
+                this.Stance = Stance.Idle;
+
+            }
+
+            // Handle the jump
+            if (Input.IsKeyDown(Keys.Space))
+            {
+                var now = Utils.GetCurrentTimeMillis();
+                if ((now > lastJump + jumpCooldown) && !this.IsAirborn())
+                {
+                    float jumpSpeed = (float)game.DeltaTime * this.JumpSpeed;
+                    velocity.Y += jumpSpeed;
+                    lastJump = now;
+                }
+            }
+
+            // Play the animation for the selected stance
+            switch (this.Stance)
+            {
+                case Stance.Walk:
+                    if (model.Animator.CurrentAnimation.Name != WalkAnimation)
+                    {
+                        model.PlayAnimation(WalkAnimation);
+                    }
+                    break;
+                case Stance.Run:
+                    if (model.Animator.CurrentAnimation.Name != RunAnimation)
+                    {
+                        model.PlayAnimation(RunAnimation);
+                    }
+                    break;
+                case Stance.Idle:
+                    if (model.Animator.CurrentAnimation.Name != IdleAnimation)
+                    {
+                        model.PlayAnimation(IdleAnimation);
+                    }
+                    break;
+                default:
+                    if (model.Animator.CurrentAnimation.Name != IdleAnimation)
+                    {
+                        model.PlayAnimation(IdleAnimation);
+                    }
+                    break;
+            }
+
+            // Set data for the next frame
+            Collider.RigidBody.Activate(true);
+            Collider.RigidBody.LinearVelocity = velocity;
+            IsRunning = false;
+            IsColliding = false;
+        }
+
+        /// <summary>
+        /// Determines whether the character is airborne.
+        /// </summary>
+        /// <returns><c>true</c> if the character is airborne; otherwise, <c>false</c>.</returns>
+        public bool IsAirborn()
+        {
+            if (this.IsColliding)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
