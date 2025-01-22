be553716e08929957dcc91df43cd047223cc3d10
Andreas Wagner <wagnerandreas17@yahoo.de>
2025-01-22 09:28:46 +0100

    Update

--- Source/Genesis/Math/Vec4.cs
+++ Source/Genesis/Math/Vec4.cs
@@ -21,5 +21,84 @@ namespace Genesis.Math
             this.Z = z;
             this.W = w;
         }
+
+        public static Vec4 operator +(Vec4 lhs, Vec4 rhs)
+        {
+            return new Vec4(lhs.X + rhs.X, lhs.Y + rhs.Y, lhs.Z + rhs.Z, lhs.W + rhs.W);
+        }
+
+        public static Vec4 operator +(Vec4 lhs, Vec3 rhs)
+        {
+            return new Vec4(lhs.X + rhs.X, lhs.Y + rhs.Y, lhs.Z + rhs.Z, lhs.W);
+        }
+
+        public static Vec4 operator +(Vec4 lhs, float rhs)
+        {
+            return new Vec4(lhs.X + rhs, lhs.Y + rhs, lhs.Z + rhs, lhs.W + rhs);
+        }
+
+        public static Vec4 operator -(Vec4 lhs, Vec4 rhs)
+        {
+            return new Vec4(lhs.X - rhs.X, lhs.Y - rhs.Y, lhs.Z - rhs.Z, lhs.W - rhs.W);
+        }
+
+        public static Vec4 operator -(Vec4 lhs, Vec3 rhs)
+        {
+            return new Vec4(lhs.X - rhs.X, lhs.Y - rhs.Y, lhs.Z - rhs.Z, lhs.W);
+        }
+
+        public static Vec4 operator -(Vec4 lhs, float rhs)
+        {
+            return new Vec4(lhs.X - rhs, lhs.Y - rhs, lhs.Z - rhs, lhs.W - rhs);
+        }
+
+        public static Vec4 operator *(Vec4 lhs, Vec4 rhs)
+        {
+            return new Vec4(lhs.X * rhs.X, lhs.Y * rhs.Y, lhs.Z * rhs.Z, lhs.W * rhs.W);
+        }
+
+        public static Vec4 operator *(Vec4 lhs, Vec3 rhs)
+        {
+            return new Vec4(lhs.X * rhs.X, lhs.Y * rhs.Y, lhs.Z * rhs.Z, lhs.W);
+        }
+
+        public static Vec4 operator *(Vec4 lhs, float rhs)
+        {
+            return new Vec4(lhs.X * rhs, lhs.Y * rhs, lhs.Z * rhs, lhs.W * rhs);
+        }
+
+        public static Vec4 operator /(Vec4 lhs, Vec4 rhs)
+        {
+            return new Vec4(lhs.X / rhs.X, lhs.Y / rhs.Y, lhs.Z / rhs.Z, lhs.W / rhs.W);
+        }
+
+        public static Vec4 operator /(Vec4 lhs, Vec3 rhs)
+        {
+            return new Vec4(lhs.X / rhs.X, lhs.Y / rhs.Y, lhs.Z / rhs.Z, lhs.W);
+        }
+
+        public static Vec4 operator /(Vec4 lhs, float rhs)
+        {
+            return new Vec4(lhs.X / rhs, lhs.Y / rhs, lhs.Z / rhs, lhs.W / rhs);
+        }
+
+        public static bool operator ==(Vec4 lhs, Vec4 rhs)
+        {
+            return lhs.X == rhs.X && lhs.Y == rhs.Y && lhs.Z == rhs.Z && lhs.W == rhs.W;
+        }
+
+        public static bool operator !=(Vec4 lhs, Vec4 rhs)
+        {
+            return lhs.X != rhs.X || lhs.Y != rhs.Y || lhs.Z != rhs.Z || lhs.W != rhs.W;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Vec4)
+            {
+                return this == (Vec4)obj;
+            }
+            return false;
+        }
     }
 }
